9c21f6cb8c7c8ab126c94a6dfb769ec8810fce63
AsyncBanana <jacobedwardjackson@outlook.com>
2022-02-06 10:07:37 -0500

    new article + improvements

--- src/lib/components/emailPrompt.svelte
+++ src/lib/components/emailPrompt.svelte
@@ -3,11 +3,7 @@
 	export let primary = true;
 </script>
 
-<div
-	class={`${
-		primary ? "text-white bg-primary" : "bg-base-200"
-	} rounded-md p-4 m-2 shadow-xl`}
->
+<div class={`bg-base-300 rounded-md p-4 m-2 shadow-xl`}>
 	<h2 class="font-bold text-xl">Sign up for updates</h2>
 	<form
 		method="POST"
@@ -17,13 +13,24 @@
 			trackEvent("Newsletter signup");
 		}}
 	>
-		<label for={`email${primary ? "-primary" : ""}`}>Enter email here</label>
+		<p>
+			Get interesting posts about web development and more programming straight
+			in your inbox!
+		</p>
+		<label for={`email${primary ? "-primary" : ""}`} class="text-md indent-2"
+			>Email</label
+		>
 		<input
 			type="email"
 			class="shadow input text-base-content"
 			name="email"
 			id={`email${primary ? "-primary" : ""}`}
+			placeholder="smart@cookie.com"
 		/>
-		<input type="submit" class="mt-4 w-min btn btn-md" />
+		<input
+			type="submit"
+			class="mt-4 w-min btn btn-md"
+			class:btn-primary={primary}
+		/>
 	</form>
 </div>
--- src/pages/author/AsyncBanana.md
+++ src/pages/author/AsyncBanana.md
@@ -3,6 +3,11 @@ layout: $layouts/AuthorLayout.astro
 name: AsyncBanana
 image: https://ik.imagekit.io/serenity/ByteofDev/Authors/AsyncBanana
 website: https://asyncbanana.dev
+email: {
+    host: gmail.com,
+    name: jej.jackson.08
+}
+github: https://github.com/AsyncBanana
 ---
 Hello! I am AsyncBanana. I am a web developer and freelance technical writer. I am also the creator of this blog.
 I should probably add more to this :)
