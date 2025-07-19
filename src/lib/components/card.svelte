e83001ee1bdd9293b2ddd0a22cf6f321ce7ff7e3
Jacob Jackson <jej.jackson.08@gmail.com>
2025-07-19 12:35:09 -0400

    site updates & microdiff article update

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -2,21 +2,24 @@
 	export let item: CollectionEntry<"posts"> | CollectionEntry<"tips">;
 	export let author: CollectionEntry<"author">;
 	export let lazy = true;
+	let className = "";
 	import type { CollectionEntry } from "astro:content";
 	import dayjs from "dayjs";
+	export { className as class };
 </script>
 
-<a
-	class="rounded-md flex flex-col shadow-md p-4 dark:bg-neutral"
-	href={`/${item.collection}/${item.slug}/`}
->
-	<img
-		src={item.data.image.url}
-		alt="Article header"
-		class="rounded-md bg-base-300 w-full aspect-video"
-		loading={lazy ? "lazy" : "eager"}
-	/>
-	<h2 class="font-bold text-xl">{item.data.title}</h2>
+<div class={`rounded-md flex flex-col m-4 ${className}`}>
+	<a href={`/${item.collection}/${item.slug}/`} class="group">
+		<img
+			src={item.data.image.url}
+			alt="Article header"
+			class="rounded-md bg-base-300 w-full aspect-video"
+			loading={lazy ? "lazy" : "eager"}
+		/>
+		<h2 class="font-bold text-3xl group-hover:opacity-85 transition-250">
+			{item.data.title}
+		</h2></a
+	>
 	<p>{item.data.description}</p>
 	<br />
 	<div style="margin-block-start: auto;">
@@ -24,9 +27,9 @@
 				By {authorData?.data.name}
 			</p>{/await}
 		<p class="font-medium">
-			{
-				dayjs(item.data.updated).format("MMMM D, YYYY")
-			}
+			{item.data.updated === item.data.published
+				? dayjs(item.data.updated).format("MMMM D, YYYY")
+				: `${dayjs(item.data.published).format("MMMM D, YYYY")} (Updated ${dayjs(item.data.updated).format("MMMM D, YYYY")})`}
 		</p>
 	</div>
-</a>
+</div>
